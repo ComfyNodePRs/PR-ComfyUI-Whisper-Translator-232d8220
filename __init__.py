fbfa15d49d6071de253d05e3eaaa7d734c7b850f
civen-cn <civen-cn@163.com>
2025-01-04 09:44:35 +0800

    delete conflict node

--- __init__.py
+++ __init__.py
@@ -1,24 +1,14 @@
-from .apply_whisper import ApplyWhisperNode
-from .add_subtitles_to_frames import AddSubtitlesToFramesNode
-from .add_subtitles_to_background import AddSubtitlesToBackgroundNode
-from .resize_cropped_subtitles import ResizeCroppedSubtitlesNode
 from .apply_whisper_x import ApplyWhisperNodeX
 from .add_subtitles_to_frames_x import AddSubtitlesToFramesNodeX
 
-NODE_CLASS_MAPPINGS = { 
-    "Apply Whisper" : ApplyWhisperNode,
+NODE_CLASS_MAPPINGS = {
     "Apply WhisperX" : ApplyWhisperNodeX,
-    "Add Subtitles To Frames": AddSubtitlesToFramesNode,
     "Add Subtitles To FramesX": AddSubtitlesToFramesNodeX,
-    "Add Subtitles To Background": AddSubtitlesToBackgroundNode,
-    "Resize Cropped Subtitles": ResizeCroppedSubtitlesNode
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-     "Apply Whisper" : "Apply Whisper", 
-     "Add Subtitles To Frames": "Add Subtitles To Frames",
-     "Add Subtitles To Background": "Add Subtitles To Background",
-     "Resize Cropped Subtitles": "Resize Cropped Subtitles"
+     "Apply WhisperX" : "Apply WhisperX",
+     "Add Subtitles To FramesX": "Add Subtitles To FramesX",
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
